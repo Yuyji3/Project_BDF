9e12b8e9bead78133fdc2efab43958db22e4e7c5
Yuyji3 <opyjho0329@naver.com>
2025-06-18 23:11:55 +0900

    1
    1

--- Assets/Script/SkillSC/SkillData.cs
+++ Assets/Script/SkillSC/SkillData.cs
@@ -6,7 +6,13 @@ public class SkillData : ScriptableObject
     public float skillnumber; // 배열 번호
     public string skillname;
     public Sprite icon;
-    public int level; // 강화 수치
+
+    public int level; // 현제 레벨
+    public int currentpower; //현재 수치
+    public int uppower; // 다음레벨 수치
+
+    [TextArea(2, 5)]
+    public string skillex; // 스킬 설명
 
     // 스킬 코드 확인용
     public GameObject skillPrefab;
