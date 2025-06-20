68941d79789b1c365d76110ab3b6c330318b49a1
Yuyji3 <opyjho0329@naver.com>
2025-06-20 19:36:04 +0900

    수정완
    수정완

--- Assets/Script/SkillSC/SkillsSC/WindCircle2.cs
+++ Assets/Script/SkillSC/SkillsSC/WindCircle2.cs
@@ -12,19 +12,16 @@ public class WindCircle2 : MonoBehaviour
 
     private CircleCollider2D myCollider;
 
-    public float minRadius = 0.5f;      // 최소 반지름
-    public float maxRadius = 4f;        // 최대 반지름
-    public float growTime = 0.1f;         // 커지는데 걸리는 시간
-    public float shrinkTime = 0.1f;
 
+    private ParticleSystem particleSystem;
     void Start()
     {
         windCircle = Tower.Instance.GetComponent<WindCircle>();
+        particleSystem = GetComponent<ParticleSystem>(); 
 
         myCollider = GetComponent<CircleCollider2D>();
         StartCoroutine(GrowShrinkRoutine());
 
-        Destroy(windCircle);
     }
 
     void Update()
@@ -34,40 +31,52 @@ public class WindCircle2 : MonoBehaviour
         {
             Destroy(windCircle);
         }
-        myCollider.radius = minRadius;
+
     }
     IEnumerator GrowShrinkRoutine()
     {
-        while (true)
-        {
-            // 커지기
-            float timer = 0f;
-            while (timer < growTime)
-            {
-                myCollider.radius = Mathf.Lerp(minRadius, maxRadius, timer / growTime);
-                timer += Time.deltaTime;
-                windCircle.Spawn();
-                yield return null;
-            }
-            myCollider.radius = maxRadius;
+        while (true) { 
 
-            windCircle.Spawn();
-
-            // 줄어들기
-            timer = 0f;
-            while (timer < shrinkTime)
-            {
-                myCollider.radius = Mathf.Lerp(maxRadius, minRadius, timer / shrinkTime);
-                timer += Time.deltaTime;
-                yield return null;
-            }
-            myCollider.radius = minRadius;
-
+            myCollider.enabled = true;
+            particleSystem.Play();
+            yield return new WaitForSeconds(1f);
+            myCollider.enabled = false;
+            particleSystem.Stop();
             yield return new WaitForSeconds(2f);
-            Destroy(windCircle);
-            yield return new WaitForSeconds(3f);
+
 
         }
+        //while (true)
+        //{
+        //    // 커지기
+        //    float timer = 0f;
+        //    while (timer < growTime)
+        //    {
+        //        myCollider.radius = Mathf.Lerp(minRadius, maxRadius, timer / growTime);
+        //        timer += Time.deltaTime;
+
+        //        yield return null;
+        //    }
+        //    myCollider.radius = maxRadius;
+
+
+        //    Debug.Log("커졌다");
+        //    // 줄어들기
+        //    timer = 0f;
+        //    while (timer < shrinkTime)
+        //    {
+        //        myCollider.radius = Mathf.Lerp(maxRadius, minRadius, timer / shrinkTime);
+        //        timer += Time.deltaTime;
+        //        yield return null;
+        //    }
+        //    myCollider.radius = minRadius;
+
+        //    yield return new WaitForSeconds(2f);
+  
+        //    Debug.Log("작아졌다");
+        //    yield return new WaitForSeconds(3f);
+
+        //}
     }
     void OnTriggerEnter2D(Collider2D other)
     {
