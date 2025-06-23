73c35ff47c76ff2dff5052f759f58772c91df6b9
Yuyji3 <opyjho0329@naver.com>
2025-06-23 17:14:22 +0900

    지금 업적 수정 미완성
    1

--- Assets/Script/GameM/AchieveManager.cs
+++ Assets/Script/GameM/AchieveManager.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 using TMPro;
 public class AchieveManager : MonoBehaviour
 {
-    [SerializeField]
-    private TextMeshProUGUI monsterKill;
 
+    public int killIndex; // 몬스터 킬 업적 단계
+    [System.Serializable]
+    public class KillAchievementInfo
+    {
+        public string name;   // "몬스터 100마리 처치"
+        public int goal;
+        public float achieve; // 보상
+
+        public TextMeshProUGUI uiText; // 달성치
+        public TextMeshProUGUI uiText2; // 업적이름
+    }
+
+    public KillAchievementInfo[] achievements;
     private void Start()
     {
-        UpdateMonsterKillUI();
+        UpdateAllAchievementUIs();
     }
-    public void UpdateMonsterKillUI()
+
+    public void UpdateAllAchievementUIs()
     {
-        monsterKill.text = $"{SaveManager.Instance.Data.monsterKillCount} / 100";
+        int killCount = SaveManager.Instance.Data.monsterKillCount;
+
+        achievements[killIndex].uiText2.text = $" {achievements[killIndex].name}";
+        achievements[killIndex].uiText2.text = $"{killCount} / {achievements[killIndex].goal}";
+
+        if (achievements[killIndex].goal <= killCount)
+        {
+            GameManager.Instance.currentBlue += achievements[killIndex].achieve;
+
+            killIndex++;
+        }
+
+
     }
 }
