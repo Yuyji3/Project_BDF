310a69782d30b0755fe51823b5e766794439d3a3
Yuyji3 <opyjho0329@naver.com>
2025-06-23 17:43:13 +0900

    업적 활성 버그

--- Assets/Script/GameM/AchieveManager.cs
+++ Assets/Script/GameM/AchieveManager.cs
@@ -10,31 +10,40 @@ public class AchieveManager : MonoBehaviour
         public string name;   // "몬스터 100마리 처치"
         public int goal;
         public float achieve; // 보상
+        public bool isClaimed; // 보상 수령 여부
 
         public TextMeshProUGUI uiText; // 달성치
         public TextMeshProUGUI uiText2; // 업적이름
     }
 
     public KillAchievementInfo[] achievements;
-    private void Start()
+
+    void Start()
     {
         UpdateAllAchievementUIs();
     }
-
     public void UpdateAllAchievementUIs()
     {
         int killCount = SaveManager.Instance.Data.monsterKillCount;
 
         achievements[killIndex].uiText2.text = $" {achievements[killIndex].name}";
-        achievements[killIndex].uiText2.text = $"{killCount} / {achievements[killIndex].goal}";
+        achievements[killIndex].uiText.text = $"{killCount} / {achievements[killIndex].goal}";
 
-        if (achievements[killIndex].goal <= killCount)
+        if (achievements[killIndex].goal <= killCount && !achievements[killIndex].isClaimed)
         {
+            gameObject.SetActive(true);
+
+            achievements[killIndex].isClaimed = true; // 수령 완료 처리
+
             GameManager.Instance.currentBlue += achievements[killIndex].achieve;
 
             killIndex++;
+
+            UpdateAllAchievementUIs(); // 문제 반복됨
+
         }
 
+        gameObject.SetActive(false);
 
     }
 }
